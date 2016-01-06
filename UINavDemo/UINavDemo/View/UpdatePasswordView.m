71797592761f3992387103a2fcc3f3ceb10b58c4
yanghf <775317846@qq.com>
2016-01-06 17:14:41 +0800

    增加显示、单选按钮、多选按钮

--- UINavDemo/UINavDemo/View/UpdatePasswordView.m
+++ UINavDemo/UINavDemo/View/UpdatePasswordView.m
@@ -10,6 +10,10 @@
 #import "DLRadioButton.h"
 
 @implementation UpdatePasswordView
+{
+    UITextField *passwordField;
+    UITextField *rePasswordField;
+}
 
 - (id)init
 {
@@ -20,8 +24,9 @@
     int padding = 10;
     self.backgroundColor = [UIColor colorWithHexString:@"eeeeee"];
     
-    UITextField *passwordField = [self textField];
+    passwordField = [self textField];
     passwordField.placeholder = @"请输入密码";
+    passwordField.secureTextEntry = YES;
     passwordField.clearButtonMode = UITextFieldViewModeWhileEditing;
     [self addSubview:passwordField];
     
@@ -32,8 +37,9 @@
         make.height.equalTo(@40);
     }];
     
-    UITextField *rePasswordField = [self textField];
+    rePasswordField = [self textField];
     rePasswordField.placeholder = @"请输入密码";
+    rePasswordField.secureTextEntry = YES;
     rePasswordField.clearButtonMode = UITextFieldViewModeWhileEditing;
     [self addSubview:rePasswordField];
     
@@ -44,6 +50,126 @@
         make.height.equalTo(@40);
     }];
 
+    DLRadioButton *radioBtn = [[DLRadioButton alloc] init];
+    radioBtn.iconSquare = YES;
+    radioBtn.iconSize = 16.0f;
+    radioBtn.iconColor = [UIColor redColor];
+    radioBtn.indicatorColor = [UIColor redColor];
+    radioBtn.titleLabel.font = [UIFont systemFontOfSize:16.0];
+    radioBtn.contentHorizontalAlignment = UIControlContentHorizontalAlignmentLeft;
+    [radioBtn addTarget:self action:@selector(actionSelRadio:) forControlEvents:UIControlEventTouchUpInside];
+    [radioBtn setTitle:@"显示密码" forState:UIControlStateNormal];
+    [radioBtn setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+    [self addSubview:radioBtn];
+    
+    [radioBtn mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(rePasswordField.mas_bottom).offset(padding);
+        make.left.equalTo(superView.mas_left).offset(padding);
+        make.width.equalTo(@120);
+        make.height.equalTo(@20);
+    }];
+    
+    
+    DLRadioButton *firstRadio = [[DLRadioButton alloc] init];
+    firstRadio.iconSize = 12;
+    firstRadio.titleLabel.font = [UIFont systemFontOfSize:16.0];
+    firstRadio.iconColor = [UIColor purpleColor];
+    firstRadio.indicatorColor = [UIColor purpleColor];
+    firstRadio.contentHorizontalAlignment = UIControlContentHorizontalAlignmentLeft;
+    [firstRadio setTitle:@"粉色" forState:UIControlStateNormal];
+    [firstRadio setTitleColor:[UIColor purpleColor] forState:UIControlStateNormal];
+    [self addSubview:firstRadio];
+    
+    [firstRadio mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(radioBtn.mas_bottom).offset(padding);
+        make.left.equalTo(superView.mas_left).offset(padding);
+        make.width.equalTo(@120);
+        make.height.equalTo(@20);
+    }];
+    
+    UIView *borderView = [[UIView alloc] init];
+    borderView = firstRadio;
+    
+    NSDictionary *radioColor = @{@"red":[UIColor redColor],@"green":[UIColor greenColor],@"blue":[UIColor blueColor]};
+    NSDictionary *radioName = @{@"red":@"红色",@"green":@"绿色",@"blue":@"蓝色"};
+    
+    NSMutableArray *radioArr = [NSMutableArray new];
+    
+    for (NSDictionary *color in radioColor) {
+        DLRadioButton *radio = [[DLRadioButton alloc] init];
+        radio.iconSize = 12;
+        radio.titleLabel.font = [UIFont systemFontOfSize:16.0];
+        radio.iconColor = [radioColor objectForKey:color];
+        radio.indicatorColor = [radioColor objectForKey:color];
+        radio.contentHorizontalAlignment = UIControlContentHorizontalAlignmentLeft;
+        [radio setTitle:[radioName objectForKey:color] forState:UIControlStateNormal];
+        [radio setTitleColor:[radioColor objectForKey:color] forState:UIControlStateNormal];
+        [self addSubview:radio];
+        
+        [radio mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.top.equalTo(borderView.mas_bottom).offset(padding);
+            make.left.equalTo(superView.mas_left).offset(padding);
+            make.width.equalTo(@120);
+            make.height.equalTo(@20);
+        }];
+        borderView = radio;
+        [radioArr addObject:radio];
+    }
+    firstRadio.otherButtons = radioArr;
+    
+    //复选框
+    NSInteger i = 1;
+    DLRadioButton *chkBtn = [[DLRadioButton alloc] init];
+    chkBtn.multipleSelectionEnabled = YES;
+    chkBtn.tag = i;
+    chkBtn.iconSize = 12;
+    chkBtn.iconSquare = YES;
+    chkBtn.titleLabel.font = [UIFont systemFontOfSize:16.0];
+    chkBtn.iconColor = [UIColor darkGrayColor];
+    chkBtn.indicatorColor = [UIColor darkGrayColor];
+    chkBtn.contentHorizontalAlignment = UIControlContentHorizontalAlignmentLeft;
+    [chkBtn addTarget:self action:@selector(actionRadio:) forControlEvents:UIControlEventTouchUpInside];
+    [chkBtn setTitle:@"浅黑" forState:UIControlStateNormal];
+    [chkBtn setTitleColor:[UIColor darkGrayColor] forState:UIControlStateNormal];
+    [self addSubview:chkBtn];
+    
+    [chkBtn mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(borderView.mas_bottom).offset(padding);
+        make.left.equalTo(superView.mas_left).offset(padding);
+        make.width.equalTo(@120);
+        make.height.equalTo(@20);
+    }];
+    
+    borderView = chkBtn;
+    
+    NSMutableArray *otherButton = [NSMutableArray new];
+    for (NSDictionary *color in radioColor) {
+        i++;
+        DLRadioButton *chkBtnOther = [[DLRadioButton alloc] init];
+        chkBtnOther.multipleSelectionEnabled = YES;
+        chkBtnOther.tag = i;
+        chkBtnOther.iconSize = 12;
+        chkBtnOther.iconSquare = YES;
+        chkBtnOther.titleLabel.font = [UIFont systemFontOfSize:16.0];
+        chkBtnOther.iconColor = [radioColor objectForKey:color];
+        chkBtnOther.indicatorColor = [radioColor objectForKey:color];
+        chkBtnOther.contentHorizontalAlignment = UIControlContentHorizontalAlignmentLeft;
+        [chkBtnOther addTarget:self action:@selector(actionRadio:) forControlEvents:UIControlEventTouchUpInside];
+        [chkBtnOther setTitle:[radioName objectForKey:color] forState:UIControlStateNormal];
+        [chkBtnOther setTitleColor:[radioColor objectForKey:color] forState:UIControlStateNormal];
+        [self addSubview:chkBtnOther];
+        
+        [chkBtnOther mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.top.equalTo(borderView.mas_bottom).offset(padding);
+            make.left.equalTo(superView.mas_left).offset(padding);
+            make.width.equalTo(@120);
+            make.height.equalTo(@20);
+        }];
+        [otherButton addObject:chkBtnOther];
+        borderView = chkBtnOther;
+
+    }
+    chkBtn.otherButtons = otherButton;
     
     return self;
 }
@@ -63,6 +189,28 @@
     textField.leftViewMode = UITextFieldViewModeAlways;
     textField.leftView = leftView;
     return textField;
+}
+
+- (void)actionSelRadio:(DLRadioButton *)radio
+{
+    if (passwordField.secureTextEntry == NO) {
+        radio.selected = NO;
+        passwordField.secureTextEntry = YES;
+        rePasswordField.secureTextEntry = YES;
+    } else {
+        radio.selected = YES;
+        passwordField.secureTextEntry = NO;
+        rePasswordField.secureTextEntry = NO;
+    }
+}
+
+- (void)actionRadio:(DLRadioButton *)radio
+{
+    if (radio.isMultipleSelectionEnabled) {
+        for (DLRadioButton *rd in radio.selectedButtons) {
+            NSLog(@"多选的值为：%ld",rd.tag);
+        }
+    }
 }
 
 @end
--- UINavDemo/vender/DLRadioButton/DLRadioButton.m
+++ UINavDemo/vender/DLRadioButton/DLRadioButton.m
@@ -37,6 +37,17 @@ static NSString *const kGeneratedIconName = @"Generated Icon";
     [self setImage:self.iconSelected forState:UIControlStateSelected | UIControlStateHighlighted];
 }
 
+- (void)setMultipleSelectionEnabled:(BOOL)multipleSelectionEnabled {
+    if (!self.isChaining) {
+        self.isChaining = YES;
+        _multipleSelectionEnabled = multipleSelectionEnabled;
+        for (DLRadioButton *radioButton in self.otherButtons) {
+            radioButton.multipleSelectionEnabled = multipleSelectionEnabled;
+        }
+        self.isChaining = NO;
+    }
+}
+
 #pragma mark - Helpers
 
 - (void)drawButton {
@@ -108,7 +119,7 @@ static NSString *const kGeneratedIconName = @"Generated Icon";
 }
 
 - (void)initRadioButton {
-    [super addTarget:self action:@selector(touchDown) forControlEvents:UIControlEventTouchDown];
+    [super addTarget:self action:@selector(touchDown) forControlEvents:UIControlEventTouchUpInside];
 }
 
 - (void)prepareForInterfaceBuilder {
@@ -125,16 +136,31 @@ static NSString *const kGeneratedIconName = @"Generated Icon";
 }
 
 - (DLRadioButton *)selectedButton {
-    if (self.selected) {
-        return self;
-    } else {
-        for (DLRadioButton *radioButton in self.otherButtons) {
-            if (radioButton.selected) {
-                return radioButton;
+    if (!self.isMultipleSelectionEnabled) {
+        if (self.selected) {
+            return self;
+        } else {
+            for (DLRadioButton *radioButton in self.otherButtons) {
+                if (radioButton.selected) {
+                    return radioButton;
+                }
             }
         }
-        return nil;
     }
+    return nil;
+}
+
+- (NSArray *)selectedButtons {
+    NSMutableArray *selectedButtons = [[NSMutableArray alloc] init];
+    if (self.selected) {
+        [selectedButtons addObject:self];
+    }
+    for (DLRadioButton *radioButton in self.otherButtons) {
+        if (radioButton.selected) {
+            [selectedButtons addObject:radioButton];
+        }
+    }
+    return selectedButtons;
 }
 
 #pragma mark - UIButton
@@ -158,9 +184,13 @@ static NSString *const kGeneratedIconName = @"Generated Icon";
 #pragma mark - UIControl
 
 - (void)setSelected:(BOOL)selected {
-    [super setSelected:selected];
-    if (selected) {
-        [self deselectOtherButtons];
+    if (self.isMultipleSelectionEnabled) {
+        [super setSelected:!self.isSelected];
+    } else {
+        [super setSelected:selected];
+        if (selected) {
+            [self deselectOtherButtons];
+        }
     }
 }
 
